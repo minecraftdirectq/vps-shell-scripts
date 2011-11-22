51ed81ed893838fba97466f5a73c3f551c12f40d
minecraftdirectq <gravypod@yahoo.com>
2011-11-22 14:22:06 -0800

    Added ALOT of stuff! phpmy admin, more php5 stuff 2 :>

--- install script.sh
+++ install script.sh
@@ -147,6 +147,14 @@ apt-get install apache2
 apt-get install php5 libapache2-mod-php5
 /etc/init.d/apache2 restart
 
+apt-get install php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+
+echo Get ready to config stuff.... Web server to reconfigure automatically: <-- apache2
+echo Configure database for phpmyadmin with dbconfig-common? <-- No
+sleep 20
+
+apt-get install phpmyadmin
+
 cd /var/www/
 wget -q -c https://github.com/downloads/minecraftdirectq/vps-shell-scripts/webfiles.zip
 
