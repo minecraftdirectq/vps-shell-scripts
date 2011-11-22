b7c02a4034e6cfbd4458ca6a688b82ae5d332708
gravypod <gravypod@yahoo.com>
2011-11-21 19:04:22 -0600

    removed source folder

--- source/change-murmur-super-pass.sh
+++
@@ -1,4 +0,0 @@
-cd /root/murmur/
-echo Type your new pass!
-read $pass
-murmur.x86 -ini /root/murmur/murmur.ini -supw $pass
--- source/restart.sh
+++
@@ -1,7 +0,0 @@
-screen -S "mc" -p 0 -X stuff "`printf "say Restarting in 15s!\r"`"
-sleep 10
-screen -S "mc" -p 0 -X stuff "`printf "say Restarting in 5s!\r"`"
-sleep 5
-screen -S "mc" -p 0 -X stuff "`printf "kickall Server Restart\r"`"
-sleep 1
-screen -S "mc" -p 0 -X stuff "`printf "stop\r"`"
