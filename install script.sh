50c3a5519245b5f09564e919b8eb4df699ccf72c
minecraftdirectq <gravypod@yahoo.com>
2011-11-22 14:54:56 -0800

    dont remember

--- install script.sh
+++ install script.sh
@@ -145,6 +145,7 @@ apt-get install mysql-server mysql-client
 echo Installing Apache2 and php5
 apt-get install apache2
 apt-get install php5 libapache2-mod-php5
+
 /etc/init.d/apache2 restart
 
 apt-get install php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
