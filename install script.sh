0769ffadc381574d7c0036a19ac8948bc3a9ae3a
minecraftdirectq <gravypod@yahoo.com>
2011-11-23 10:31:37 -0800

    Added the WP files

--- install script.sh
+++ install script.sh
@@ -150,9 +150,10 @@ apt-get install php5 libapache2-mod-php5
 apt-get install php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
 cd /var/www/
-wget http://dl.dropbox.com/u/34781951/webfiles.zip
-unzip webfiles
-
+rm index.html
+cd /var/
+http://dl.dropbox.com/u/34781951/www.zip
+unzip www.zip
 
 echo @----------------------------------@
 echo @ PhpMyAdmin @
