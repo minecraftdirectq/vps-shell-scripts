6ce9b1a26bc6233018338e9d1b80569ad9d6475e
James NixonInnes <nixoninnes@gmail.com>
2011-11-23 18:57:26 +0000

    added a few comments and tidied some of the syntax

--- install+choose dir.sh
+++ install+choose dir.sh
@@ -1,17 +1,28 @@
-#!/bin/bash
-echo ""
-echo "@-------------------------------------------@"
-echo "@     Tyrantelf's Shell Server settup       @"
-echo "@        Credits                            @" 
-echo "@        -Khobbits                          @"
-echo "@        -Darklust                          @"
-echo "@        -GravyPod                          @"
-echo "@-------------------------------------------@"
+!/bin/bash
+clear
+echo @-------------------------------------------@
+echo @        BUKKIT SERVER SETUP SCRIPT         @
+echo @-------------------------------------------@
+echo @        Credits                            @ 
+echo @          -Khobbits                        @
+echo @          -Darklust                        @
+echo @          -GravyPod                        @
+echo @          -Tyrantelf                       @
+echo @          -NixonInnes                      @
+echo @-------------------------------------------@
 
 ####################################################
-# Loading Funcitons
+# Default parameters
+####################################################
+D_INSTALLDIR = /opt/minecraft/
+
+
+####################################################
+# Functions
 ####################################################
 counter=1
+
+# Dots loading 
 function dots {
 while [ $counter -le 3 ]
 do
@@ -23,7 +34,7 @@ let counter=1
 echo
 }
 
-#[LOADING BAR]#
+# Loading bar
 function lbar {
 while [ $counter -le 3 ]
 do
@@ -35,23 +46,62 @@ let counter=1
 echo
 }
 
+# Pause function
+function pause() {
+   read -p "$*"
+}
+
 ####################################################
-# Choose install Dir and install necessary packages
 ####################################################
 
-echo "Absolute Directory Path (/path/to/directory) for install:"
-read installdir
+####################################################
+# INITIALISATION
+####################################################
+
+####################################################
+# Check if the script is being run by a root user
+####################################################
+
+if [[ $EUID -ne 0 ]]; then
+   echo "You should run this script as a root user, or using 'sudo'." 1>&2
+   exit 1
+fi
+
+
+####################################################
+# CONFIGURATION
+####################################################
+
+# Choose install dir and install necessary packages
+echo "The install will default to /opt/minecraft, if you wish to"
+echo "use a custom directory please enter your ABSOLUTE"
+echo "directory path (/path/to/directory) for install:"
+echo "(leave blank, and hit return to accept default)"
+read INSTALLDIR
+
+# If INSTALLDIR is empty set  to default.
+if [ -z "$INSTALLDIR" ]; then
+  INSTALLDIR = D_INSTALLDIR
+fi
+
+# Check if INSTALLDIR exists, if not then create it
+if [ ! -d "$INSTALLDIR" ]; then
+  mkdir $INSTALLDIR
+fi
+
 
 echo @----------------------------------@
 echo @          INSTALLING STUFF        @
 echo @----------------------------------@
 
-echo "Prepare for lots of text, click "y" if prompted!"
-echo "If your not running this as root, you will need to enter your password."
-sleep 10
+echo Preparing to install packages;"
+echo "you may be required to press 'y' if prompted"
+pause "Press any key to continue..."
+# this next line is ugly, we should just crack in a new repo. A better solution would be to check the OS version and add the appropriate package -nix
 sudo cat deb http://archive.canonical.com/ lucid partner >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install sun-java6-jre sun-java6-plugin
+# im not sure if this is needed, it is done by default
 sudo update-java-alternatives -s java-6-sun
 sudo apt-get install screen
 sudo apt-get install rdiff
@@ -61,10 +111,12 @@ sudo apt-get install unzip
 sudo gnome-terminal -x sudo update-java-alternatives -s java-6-sun
 sudo apt-get update
 
+
 echo @----------------------------------@
 echo @          DLING THINGS :P         @
 echo @----------------------------------@
 
+# potentially include these files with the setup script, rather than downloading them. This way if in the future we can taylor packages accordingly by simply modifying the setup script. -nix
 wget -q -c https://github.com/downloads/minecraftdirectq/vps-shell-scripts/scriptneed.zip
 
 lbar
@@ -73,6 +125,8 @@ sudo mv scriptneed.zip $installdir/
 lbar
 unzip scriptneed.zip
 lbar
+
+# should be checks before this to make sure they dont already exist
 sudo mkdir $installdir/testerver
 sudo mkdir $installdir/minecraft
 sudo mkdir $installdir/backups
@@ -86,6 +140,8 @@ echo "@    Script Downloading done!!!     @"
 echo "------------------------------------"
 
 dots
+
+# probably better practise to use absolute references, just in case.. -nix
 cd $installdir/scipts/
 sudo chmod +x start.sh
 sudo chmod +x run.sh
@@ -109,8 +165,10 @@ echo Editing cron!
 dots
 echo Dumping file!
 crontab -l > /tmp/crondump
-echo Inserting jobes into cron!
+echo Inserting jobs into cron!
 dots
+
+# I think that the server management should be handled by a single script installed as a service, so it can be managed simply via "service bukkit start/stop/restart/update/backup" this will also make unning on startup cleaner. -nix
 sudo cat @reboot $installdir/start.sh >> /tmp/crondump
 sudo cat 0 0 * * 0 $installdir/stop.sh >> /tmp/crondump
 sudo cat 0 * * * * $installdir/restart.sh >> /tmp/crondump
@@ -121,16 +179,17 @@ sleep 5
 clear
 
 
-echo @----------------------------------@
-echo @          Server startup          @
-echo @----------------------------------@
+echo @---------------------------------------------@
+echo @          Configuring Bukkit Server          @
+echo @---------------------------------------------@
 
 dots
 
-echo Minimum RAM in Megabytes:
+# We should add some helpful info for the user in here; see cat /proc/meminfo -nix
+echo Minimum RAM in Megabytes to allocate server:
 read ram
 
-echo Max Ram in Megabytes:
+echo Maximum Ram in Megabytes to allocate server:
 read ram2
 
 sed -i 's/  java -server -Xms1024M -Xmx2250M -jar craftbukkit.jar/  java -server -Xms$ramM -Xmx$ram2M -jar craftbukkit.jar/g' /root/scripts/run.sh
@@ -138,7 +197,7 @@ sed -i 's/  java -server -Xms1024M -Xmx2250M -jar craftbukkit.jar/  java -server
 echo Adding lots of alias.
 echo “alias startall='$installdir/start.sh'” >> ~/.profile
 
-
+# I think the following mysql / php / httpd/apache should be an optional install -nix
 echo @----------------------------------@
 echo @           mysql/apache           @
 echo @----------------------------------@
@@ -184,3 +243,4 @@ echo "Rebooting To Finnish :>!"
 dots
 
 reboot
+# ? shutdown -r now -nix
