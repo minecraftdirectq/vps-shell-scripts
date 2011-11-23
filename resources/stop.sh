8dffcd6a9db100bf5f9b776cac26c3bf12edc85e
minecraftdirectq <gravypod@yahoo.com>
2011-11-22 16:18:59 -0800

    added the `"'s back, u need them tyr :P

--- resources/stop.sh
+++ resources/stop.sh
@@ -1,10 +1,10 @@
 screen -S "mc" -p 0 -X stuff "`printf "Full server restart! Server will restart in 10 secconds!\r"`"
 sleep 10
-screen -S "mc" -p 0 -X stuff "`printf "Full stop!!!\r"
+screen -S "mc" -p 0 -X stuff "`printf "Full stop!!!\r"`"
 sleep 1
-screen -S "mc" -p 0 -X stuff "`kickall Server restart, some back in 5-10 minutes\r"
+screen -S "mc" -p 0 -X stuff "`kickall Server restart, some back in 5-10 minutes\r"`"
 sleep 1
-screen -S "mc" -p 0 -X stuff "`printf "stop\r"
+screen -S "mc" -p 0 -X stuff "`printf "stop\r"`"
 sleep 2
 screen -p 0 -X kill
 sleep 2
