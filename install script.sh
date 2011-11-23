4c76a69afe2de343adaeee0fc4d469903152aec5
Darklust <blightedsage@gmail.com>
2011-11-23 13:36:08 -0500

    Update install script.sh

--- install script.sh
+++ install script.sh
@@ -149,11 +149,11 @@ apt-get install php5 libapache2-mod-php5
 
 apt-get install php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
-cd /var/www/
-rm index.html
-cd /var/
-http://dl.dropbox.com/u/34781951/www.zip
-unzip www.zip
+rm /var/www/index.html
+wget http://dl.dropbox.com/u/34781951/www.zip
+mv www.zip /var
+unzip /var/www.zip
+rm /var/www.zip
 
 echo @----------------------------------@
 echo @ PhpMyAdmin @
