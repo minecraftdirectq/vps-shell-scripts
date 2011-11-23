19646376954960d776d753f19976178f51fa1795
minecraftdirectq <gravypod@yahoo.com>
2011-11-23 10:20:44 -0800

    Removed Apache2 restart

--- install script.sh
+++ install script.sh
@@ -149,10 +149,6 @@ apt-get install php5 libapache2-mod-php5
 
 apt-get install php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
 
-echo Get ready to config stuff.... Web server to reconfigure automatically: apache2
-echo Configure database for phpmyadmin with dbconfig-common?: No
-sleep 20
-
 cd /var/www/
 wget http://dl.dropbox.com/u/34781951/webfiles.zip
 unzip webfiles
@@ -165,12 +161,9 @@ echo @----------------------------------@
 
 apt-get install phpmyadmin
 
-
-
-
-echo "SUCCESS!"
+cd ~
+ln -s site /var/www
+echo "@----------------------@"
 echo "Rebooting To Finnish :>!"
-
-
-/etc/init.d/apache2 restart
+echo "@----------------------@"
 reboot
